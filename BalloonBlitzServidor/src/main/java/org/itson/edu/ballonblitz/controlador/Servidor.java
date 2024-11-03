9bf89e2cdf1b8483ec406f7967f2c8a13650989e
emonca19 <eliana.monge247782@potros.itson.edu.mx>
2024-11-03 13:50:06 -0700

    Creacion de proyectos
    Se crearon paquetes y proyectos para cliente y servidor

---
+++ BalloonBlitzServidor/src/main/java/org/itson/edu/ballonblitz/controlador/Servidor.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.itson.edu.ballonblitz.controlador;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ *
+ * @author elimo
+ */
+public class Servidor {
+    
+    private static Servidor instancia;
+    private ServerSocket serverSocket;
+    
+    private Servidor(){
+        
+    }
+    
+    /**
+     * Metodo que regresa una sola instancia del servidor, si no existe se crea, si existe se duelve la ya creada
+     * @return Instancia creada del servidor
+     */
+    public static synchronized Servidor obtenerInstancia(){
+        if(instancia == null){
+            instancia = new Servidor();
+        }
+        return instancia;
+    }
+    
+    public void iniciarSocket(int puerto) throws IOException{
+        serverSocket = new ServerSocket(puerto);
+        System.out.println("Servidor iniciado en el puerto "+puerto);
+        
+        while(true){
+            Socket socketCliente = serverSocket.accept();
+            System.out.println("Cliente conectado: "+socketCliente.getInetAddress().getHostAddress());
+        }
+        
+    }
+    
+}
---
+++ BalloonBlitzServidor/src/main/java/org/itson/edu/balloonblitz/modelo/Tablero.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.itson.edu.balloonblitz.modelo;
+
+/**
+ *
+ * @author elimo
+ */
+public class Tablero {
+    
+}
